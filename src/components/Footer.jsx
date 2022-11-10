b4cf11b0bf3a93f188cfc017fa12e71fa4326df8
AXELMINART <77530883+Axel-cmd@users.noreply.github.com>
2022-11-10 10:56:11 +0100

    Footer changer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,14 @@ import {
   MDBCol,
   MDBRow,
 } from 'mdb-react-ui-kit';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../contexts/auth.context';
 
-export default function App() {
+const Footer = () => {
+
+
+  const { token } = useAuth();
+
   return (
     <MDBFooter className='text-center' color='white' bgColor='dark'>
       <MDBContainer className='p-4'>
@@ -21,57 +27,57 @@ export default function App() {
 
         <section className=''>
           <MDBRow center>
+            <h5 className='text-uppercase'>Liens du site</h5>
             <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-              <h5 className='text-uppercase'>Links</h5>
 
               <ul className='list-unstyled mb-0'>
                 <li>
-                  <a href='#!' className='text-white'>
-                    Link 1
+                  <a href='/' className='text-white'>
+                    Accueil
                   </a>
+
                 </li>
                 <li>
-                  <a href='#!' className='text-white'>
-                    Link 2
+                  <a href='/products' className='text-white'>
+                    Produits
                   </a>
                 </li>
-                <li>
-                  <a href='#!' className='text-white'>
-                    Link 3
-                  </a>
-                </li>
-                <li>
-                  <a href='#!' className='text-white'>
-                    Link 4
-                  </a>
-                </li>
+                
               </ul>
             </MDBCol>
 
             <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-              <h5 className='text-uppercase'>Links</h5>
 
               <ul className='list-unstyled mb-0'>
-                <li>
-                  <a href='#!' className='text-white'>
-                    Link 1
-                  </a>
-                </li>
-                <li>
-                  <a href='#!' className='text-white'>
-                    Link 2
-                  </a>
-                </li>
-                <li>
-                  <a href='#!' className='text-white'>
-                    Link 3
-                  </a>
-                </li>
-                <li>
-                  <a href='#!' className='text-white'>
-                    Link 4
-                  </a>
-                </li>
+
+                {token ? (
+                  <>
+                    <li>
+                      <a href='/MyProfil' className='text-white'>
+                        Mon profil
+                      </a>
+                    </li>
+                    <li>
+                      <a href='/' className='text-white'>
+                        Mes annonces
+                      </a>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li>
+                      <a href='/login' className='text-white'>
+                        Connexion
+                      </a>
+                    </li>
+                    <li>
+                      <a href='/register' className='text-white'>
+                        Créer un compte
+                      </a>
+                    </li>
+                  </>
+                )}
+
               </ul>
             </MDBCol>
           </MDBRow>
@@ -79,11 +85,13 @@ export default function App() {
       </MDBContainer>
 
       <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2022 Copyright:
+        © 2022 Copyright:  
         <a className='text-white' href='http://localhost:3000/'>
-          Mon-Annonce
+          Mon Annonce
         </a>
       </div>
     </MDBFooter>
-  );
+  )
 }
+
+export default Footer
