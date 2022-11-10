4e0487b9fd6bf81981cfcb615071c60471f7fcb5
neko49 <noudehouen-regis-jaures.fassinou@estiam.com>
2022-11-10 15:05:36 +0100

    Modification Page Profil admin

--- src/pages/MyProfil.jsx
+++ src/pages/MyProfil.jsx
@@ -1,8 +1,149 @@
+import React, { useEffect } from 'react';
+import {
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBCard,
+  MDBCardText,
+  MDBCardBody,
+  MDBCardImage,
+  MDBBtn,
+  MDBBreadcrumb,
+  MDBBreadcrumbItem,
+  MDBProgress,
+  MDBProgressBar,
+  MDBIcon,
+  MDBListGroup,
+  MDBListGroupItem
+} from 'mdb-react-ui-kit';
+import { useAuth } from '../contexts/auth.context';
+import { getUploadedFile } from '../api/offer';
+import { useState } from 'react';
 
+export default function MyProfil() {
+    const {user} = useAuth();
+    const [image, setImage] = useState(null);
+    const [pieceIenditite, setPieceIdentite] = useState(null);
 
-const MyProfil = () => {
-
-    return <h1>MyProfil</h1>
+    useEffect(() => {
+        getUploadedFile(user.profile_picture)
+        .then(value => {
+            const url = window.URL || window.webkitURL;
+            const blobUrl = url.createObjectURL(value);
+            setImage(blobUrl)
+        })
+        getUploadedFile(user.identifical_file)
+        .then(value => {
+            const url = window.URL || window.webkitURL;
+            const blobUrl = url.createObjectURL(value);
+            setPieceIdentite(blobUrl)
+        })}
+        
+    )
+  return (
+    <section style={{ backgroundColor: '#eee' }}>
+      <MDBContainer className="py-5">
+        <MDBRow>
+          <MDBCol lg="4">
+            <MDBCard className="mb-4">
+              <MDBCardBody className="text-center">
+              <MDBCardImage
+                  src={image}
+                  alt="avatar"
+                  className="rounded-circle"
+                  style={{ width: '50px' }}
+                  fluid /><hr/>
+                <MDBCardImage
+                  src={pieceIenditite}
+                  alt="avatar"
+                  className="rounded-circle"
+                  style={{ width: '50px' }}
+                  fluid /><hr/>
+                <div className="d-flex justify-content-center mb-2">
+                  <MDBBtn>Modifier Profil</MDBBtn>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+          <MDBCol lg="8">
+            <MDBCard className="mb-4">
+              <MDBCardBody>
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Firstname</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">{user.firstname}</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Lastname</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">{user.lastname}</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Email</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">{user.email}</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Téléphone</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">{user.phone}</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Adresse Postale</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">{user.address}</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Code Postale</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">{user.zip_code}</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Ville</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">{user.city}</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Pays</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">{user.country}</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </section>
+  );
 }
-
-export default MyProfil;
