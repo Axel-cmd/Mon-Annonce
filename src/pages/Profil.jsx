1695152f7aa3fb69ce4e1226b3d4c1ccbca937ea
neko49 <noudehouen-regis-jaures.fassinou@estiam.com>
2022-11-10 12:50:24 +0100

    Modif Profil

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBTypography } from 'mdb-react-ui-kit';
 import { getUserProfileById } from '../api/user';
-import { getPublicOfferByUserId } from '../api/offer';
+import { getPublicOfferByUserId, getUploadedFile } from '../api/offer';
 import { useParams } from 'react-router-dom';
 export default function Profil() {
     const [offer, setOffer] = useState();
     const [user, setUser] = useState();
+    const [image, setImage] = useState(null);
     const {id} = useParams();
 
 
@@ -14,6 +15,14 @@ export default function Profil() {
             .then(res => {
                 console.log(res)
                 setUser(res)
+                if(res && res.profile_picture){
+
+                    getUploadedFile(res.profile_picture)
+                        .then(value => {
+                            const url = window.URL || window.webkitURL;
+                            const blobUrl = url.createObjectURL(value);
+                            setImage(blobUrl)
+                        })}
             })
             .catch(err => console.log(err))
     }, [id])
@@ -36,12 +45,13 @@ export default function Profil() {
             <MDBCard>
               <div className="rounded-top text-white d-flex flex-row" style={{ backgroundColor: '#000', height: '200px' }} >
                 <div className="ms-4 mt-5 d-flex flex-column" style={{ width: '150px' }}>
-                  <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                  <MDBCardImage src={image??"https://th.bing.com/th/id/R.7f70ccdaea42a3ea7f34f470040c1b29?rik=XpAJhC2Net1BXg&pid=ImgRaw&r=0"}
                     alt="Generic placeholder image" className="mt-4 mb-2 img-thumbnail" fluid style={{ width: '150px', zIndex: '1' }} />
                 </div>
                 <div className="ms-3" style={{ marginTop: '130px' }}>
-                  <MDBTypography tag="h5">Andy Horwitz</MDBTypography>
-                  <MDBCardText>New York</MDBCardText>
+                  <MDBTypography tag="h5">{user? user.firstname:''}</MDBTypography>
+                  <MDBTypography tag="h5">{user? user.lastname:''}</MDBTypography>
+                  
                 </div>
               </div>
               <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
@@ -61,14 +71,7 @@ export default function Profil() {
                 </div>
               </div>
               <MDBCardBody className="text-black p-4">
-                <div className="mb-5">
-                  <p className="lead fw-normal mb-1">About</p>
-                  <div className="p-4" style={{ backgroundColor: '#f8f9fa' }}>
-                    <MDBCardText className="font-italic mb-1">Web Developer</MDBCardText>
-                    <MDBCardText className="font-italic mb-1">Lives in New York</MDBCardText>
-                    <MDBCardText className="font-italic mb-0">Photographer</MDBCardText>
-                  </div>
-                </div>
+
                 <div className="d-flex justify-content-between align-items-center mb-4">
                   <MDBCardText className="lead fw-normal mb-0">Annonce Récent</MDBCardText>
                   <MDBCardText className="mb-0"><a href="#!" className="text-muted">Voir Plus</a></MDBCardText>
